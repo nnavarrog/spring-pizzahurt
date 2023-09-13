8c2d07c79be334d947d4d251b0c861bc937e9075
nnavarrog <nnavarrogutierrez@gmail.com>
2023-09-13 18:44:45 -0300

    project structure

---
+++ src/main/java/uy/edu/ort/obligatorio/pizzahurt/PizzahurtApplication.java
@@ -0,0 +1,13 @@
+package uy.edu.ort.obligatorio.pizzahurt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzahurtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzahurtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uy/edu/ort/obligatorio/pizzahurt/PizzahurtApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.ort.obligatorio.pizzahurt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzahurtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
