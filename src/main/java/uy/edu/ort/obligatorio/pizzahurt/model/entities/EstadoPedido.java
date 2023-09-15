f05e628c72f3bef578a92fa83215e5ed3ed259de
nnavarrog <nnavarrogutierrez@gmail.com>
2023-09-15 14:20:40 -0300

    Enum de estados pronto

---
+++ src/main/java/uy/edu/ort/obligatorio/pizzahurt/model/entities/EstadoPedido.java
@@ -0,0 +1,19 @@
+/*
+ * NO LICENCE 
+ * Author: Ing. Nicolás Navarro Gutérrez
+ */
+package uy.edu.ort.obligatorio.pizzahurt.model.entities;
+
+/**
+ *
+ * @author nnavarro
+ */
+public enum EstadoPedido
+{
+    NUEVO,
+    PENDIENTE_PAGO,
+    PENDIENTE_ENVIO,
+    ENVIADO,
+    RECHAZADO,
+    ENTREGADO
+}
